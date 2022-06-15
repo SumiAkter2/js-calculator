3a43cd4e2aa67bea877e463c36e8aa149cfaf605
SumiAkter2 <akteralisha36@gmail.com>
2022-06-15 08:29:22 +0600

    added cal

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 console.log('js');
-const show1 = document.getElementById('one').addEventListener('click', function () {
-    const inputValue = document.getElementById('input').value;
-    console.log(inputValue);
-    console.log('one');
+
+const input = document.getElementById('input');
+
+const buttons = Array.from(document.getElementsByClassName('btn'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+
+        switch (e.target.innerText) {
+            case 'AC':
+                input.innerText = '';
+                break;
+            case 'x':
+                if (input.innerText) {
+                    input.innerText = input.innerText.slice(0, -1);
+                }
+
+                break;
+            case '=':
+                try {
+                    input.innerText = eval(input.innerText);
+                }
+                catch {
+                    input.innerText = 'ERROR!';
+                }
+                break;
+            default:
+                input.innerText += e.target.innerText;
+        }
+        if (input.innerText.length > 34) {
+            input.innerText = 'Error! ';
+        }
+    })
 })
-const show2 = document.getElementById('two').addEventListener('click', function () {
-    console.log('two');
-})
-const show3 = document.getElementById('three').addEventListener('click', function () {
-    console.log('three');
-})
-const show4 = document.getElementById('four').addEventListener('click', function () {
-    console.log('four');
-})
-const show5 = document.getElementById('five').addEventListener('click', function () {
-    console.log('five');
-})
-const show6 = document.getElementById('six').addEventListener('click', function () {
-    console.log('six');
-})
-const show7 = document.getElementById('seven').addEventListener('click', function () {
-    console.log('seven');
-})
-const show8 = document.getElementById('eight').addEventListener('click', function () {
-    console.log('eight');
-})
-const show9 = document.getElementById('nine').addEventListener('click', function () {
-    console.log('nine');
-})
-const showplus = document.getElementById('plus').addEventListener('click', function () {
-    console.log('plus');
-})
-const showMinus = document.getElementById('minus').addEventListener('click', function () {
-    console.log('minus');
-})
-const showMulti = document.getElementById('multi').addEventListener('click', function () {
-    console.log('multi');
-})
-const showDivide = document.getElementById('divide').addEventListener('click', function () {
-    console.log('divide');
-})
+console.log('hi');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
